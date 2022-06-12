e32752a46f144547a66ac6ffdba5f85d097c0e12
Alice  Cutter <cuttera@carleton.edu>
2022-06-12 17:20:44 -0400

    push

--- node.py
+++ node.py
@@ -3,4 +3,6 @@ class Path:
     ''' a node class that will store neighbors in an adgacency  matrix'''
     def __init__(self, neighbors):
         self.weight = 0
-        self.
+        print(self.weight)
+        
+    
